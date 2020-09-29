4b5bfcc4a949fdbf20992d002a9f0724cd572197
Ardyansyah Vira B <54975146+ardyansyah5711@users.noreply.github.com>
2020-09-29 12:06:02 +0700

    Update Readme dan Layout

---
+++ README.md
@@ -0,0 +1,4 @@
+#  APLIKASI INPUT DATA MAHASISWA
+
+Ardyansyah Vira B   1841720180
+Unero Bhagaskara Ramadhan M     1841720173
---
+++ app/src/main/java/com/unero/activity/TampilData.java
@@ -0,0 +1,14 @@
+package com.unero.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TampilData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil_data);
+    }
+}
